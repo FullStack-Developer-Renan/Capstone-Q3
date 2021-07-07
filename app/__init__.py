7efa1a67a2846fc7964f657441d64b2b8bdf07cc
Junia Nunes <jununes.bermejo@gmail.com>
2021-07-06 21:00:33 -0300

    feat(app/models/orders_model): create orders_model

--- app/__init__.py
+++ app/__init__.py
@@ -8,10 +8,10 @@ def create_app() -> Flask:
     env.read_env
 
     app = Flask(__name__)
-    
-    app.config['SQLALCHEMY_DATABASE_URI'] = env('DATABASE_URI')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['JSON_SORT_KEYS'] = False
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = env("DATABASE_URI")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["JSON_SORT_KEYS"] = False
 
     database.init_app(app)
     migration.init_app(app)
