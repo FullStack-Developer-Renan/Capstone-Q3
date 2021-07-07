e9d6b300ac5d68e73a2e9c2ae7e3fb5b914f1910
Junia Nunes <jununes.bermejo@gmail.com>
2021-07-07 15:43:31 -0300

    feat(app/services/table_service.py): add function delete_table and update_table

--- app/services/tables_service.py
+++ app/services/tables_service.py
@@ -1,11 +1,34 @@
-from flask import jsonify
+from flask import jsonify, request
 from .helpers import add_commit
 from app.models.restaurant_table_model import RestaurantTableModel
+from http import HTTPStatus
 
 
-def create_table(data: dict) -> dict:
+def create_table(data: dict):
     new_table = RestaurantTableModel(**data)
 
     add_commit(new_table)
 
     return jsonify(new_table)
+
+
+def delete_table(table_id: int):
+    found_table = RestaurantTableModel.query.get(table_id)
+
+    if not found_table:
+        return {"status": "table not found"}, HTTPStatus.NOT_FOUND
+
+    add_commit(found_table)
+
+    return "", HTTPStatus.NO_CONTENT
+
+
+def update_table(data: dict, table_id: int):
+       
+    found_table = RestaurantTableModel.query.get(table_id)
+    
+    if not found_table:
+        return {"status": "table not found"}, HTTPStatus.NOT_FOUND
+
+
+    
