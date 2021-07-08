478564152f45f9b49eb466328e8e7b4004f3e077
Junia Nunes <jununes.bermejo@gmail.com>
2021-07-07 21:26:02 -0300

    feat(app/services/table_service.py): add function update table

--- app/services/helpers.py
+++ app/services/helpers.py
@@ -1,7 +1,14 @@
 from flask import current_app
 
 
+session = current_app.db.session
+
+
 def add_commit(model) -> None:
-    session = current_app.db.session
     session.add(model)
+    session.commit()
+
+
+def delete_commit(model) -> None:
+    session.delete(model)
     session.commit()
--- app/services/tables_service.py
+++ app/services/tables_service.py
@@ -1,5 +1,5 @@
 from flask import jsonify, request
-from .helpers import add_commit
+from .helpers import add_commit, delete_commit
 from app.models.restaurant_table_model import RestaurantTableModel
 from http import HTTPStatus
 
@@ -12,23 +12,48 @@ def create_table(data: dict):
     return jsonify(new_table)
 
 
+def login_table():
+    ...
+
+
 def delete_table(table_id: int):
     found_table = RestaurantTableModel.query.get(table_id)
 
     if not found_table:
         return {"status": "table not found"}, HTTPStatus.NOT_FOUND
 
-    add_commit(found_table)
+    delete_commit(found_table)
 
     return "", HTTPStatus.NO_CONTENT
 
 
 def update_table(data: dict, table_id: int):
-       
+
     found_table = RestaurantTableModel.query.get(table_id)
-    
+
     if not found_table:
         return {"status": "table not found"}, HTTPStatus.NOT_FOUND
 
+    for key, value in data.items():
+        setattr(found_table, key, value)
 
-    
+    add_commit(found_table)
+
+    return jsonify(found_table)
+
+
+def get_table(table_id: int):
+    found_table_id = RestaurantTableModel.query.filter(
+        RestaurantTableModel.id == table_id
+    ).all()
+
+    return found_table_id
+
+
+# tables -> GET POST PATCH
+# endpoint(GET_TABLES) = '/api/tables?empty=<empty: bool>/' -> GET
+
+# endpoint(GET_TABLE) = '/api/tables/<table_id: int>/' -> GET
+
+
+# endpoint(LOGIN_TABLE) = '/api/tables/login' -> POST
