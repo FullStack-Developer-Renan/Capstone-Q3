90d07e72798854329073a26666d1313a69d56dc8
Junia Nunes <jununes.bermejo@gmail.com>
2021-07-08 14:17:32 -0300

    feat(app/services/table_service.py): refactor table services

--- app/services/helpers.py
+++ app/services/helpers.py
@@ -7,8 +7,3 @@ session = current_app.db.session
 def add_commit(model) -> None:
     session.add(model)
     session.commit()
-
-
-def delete_commit(model) -> None:
-    session.delete(model)
-    session.commit()
--- app/services/tables_service.py
+++ app/services/tables_service.py
@@ -1,59 +1,97 @@
-from flask import jsonify, request
-from .helpers import add_commit, delete_commit
+from flask import jsonify, current_app
+from .helpers import add_commit
+from flask_restful import reqparse
 from app.models.restaurant_table_model import RestaurantTableModel
 from http import HTTPStatus
 
 
-def create_table(data: dict):
-    new_table = RestaurantTableModel(**data)
+def get_by_id(table_id) -> RestaurantTableModel:
+    table = RestaurantTableModel.query.get(table_id)
+    if table:
+        return table, HTTPStatus.OK
+    return {}, HTTPStatus.NOT_FOUND
+
+
+# endpoint(CREATE_TABLE) = '/api/tables/' -> POST
+def create_table() -> RestaurantTableModel:
+    parser = reqparse.RequestParser()
+
+    parser.add_argument("seats", type=int, required=True)
+    parser.add_argument("number", type=int, required=True)
+    parser.add_argument("total", type=int, required=True)
+    parser.add_argument("empty", type=bool, required=True)
+    parser.add_argument("password", type=str, required=True)
+
+    password = parser.pop("password")
+    new_table: RestaurantTableModel = RestaurantTableModel(
+        **parser.parse_args(strict=True)
+    )
+    new_table.password = password
 
     add_commit(new_table)
 
-    return jsonify(new_table)
+    return new_table
 
 
 def login_table():
     ...
 
 
+# endpoint(DELETE_TABLE) = '/api/tables/<table_id: int>/' -> DELETE
 def delete_table(table_id: int):
+    session = current_app.db.session
+
     found_table = RestaurantTableModel.query.get(table_id)
 
     if not found_table:
         return {"status": "table not found"}, HTTPStatus.NOT_FOUND
 
-    delete_commit(found_table)
+    session.delete(found_table)
 
     return "", HTTPStatus.NO_CONTENT
 
 
-def update_table(data: dict, table_id: int):
+# endpoint(UPDATE_TABLE) = '/api/tables/<table_id: int>/' -> PATCH
+def update_table(table_id: int) -> RestaurantTableModel:
+    parser = reqparse.RequestParser()
 
-    found_table = RestaurantTableModel.query.get(table_id)
+    parser.add_argument("seats", type=int, required=False)
+    parser.add_argument("number", type=int, required=False)
+    parser.add_argument("total", type=int, required=False)
+    parser.add_argument("empty", type=bool, required=False)
+    parser.add_argument("password", type=str, required=False)
 
-    if not found_table:
+    table = get_by_id(table_id)
+
+    if not table:
         return {"status": "table not found"}, HTTPStatus.NOT_FOUND
 
-    for key, value in data.items():
-        setattr(found_table, key, value)
+    for key, value in parser.parse_args(strict=True):
+        setattr(table, key, value)
 
-    add_commit(found_table)
+    add_commit(table)
 
-    return jsonify(found_table)
+    return table, HTTPStatus.OK
 
 
+# endpoint(GET_TABLE) = '/api/tables/<table_id: int>/' -> GET
 def get_table(table_id: int):
-    found_table_id = RestaurantTableModel.query.filter(
-        RestaurantTableModel.id == table_id
-    ).all()
+    parser = reqparse.RequestParser()
 
-    return found_table_id
+    parser.add_argument("seats", type=int, required=True)
+    parser.add_argument("number", type=int, required=True)
+    parser.add_argument("total", type=int, required=True)
+    parser.add_argument("empty", type=bool, required=True)
+    parser.add_argument("password", type=str, required=True)
+
+    table = get_by_id(table_id)
+
+    add_commit(parser)
+
+    return table, HTTPStatus.OK
 
 
 # tables -> GET POST PATCH
+
 # endpoint(GET_TABLES) = '/api/tables?empty=<empty: bool>/' -> GET
-
-# endpoint(GET_TABLE) = '/api/tables/<table_id: int>/' -> GET
-
-
 # endpoint(LOGIN_TABLE) = '/api/tables/login' -> POST
