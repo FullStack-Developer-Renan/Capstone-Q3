93ac652113315c4f8e9a6a080159b2a4150b7062
Junia Nunes <jununes.bermejo@gmail.com>
2021-07-14 23:20:52 -0300

    feat(app/views/tables_services.py): add erros and serialize

--- app/views/tables_view.py
+++ app/views/tables_view.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource
 from http import HTTPStatus
-
+from flask_jwt_extended import jwt_required
+from sqlalchemy.exc import IntegrityError
 
 from app.services.tables_services import (
     get_tables,
@@ -13,24 +14,32 @@ from app.services.tables_services import (
 
 
 class TablesResource(Resource):
+    @jwt_required()
     def get(self):
         return get_tables()
 
+    @jwt_required()
     def post(self):
         return create_table()
 
 
 class TableIdResource(Resource):
+    @jwt_required()
     def get(self, id: int):
         return get_by_id(id)
 
+    @jwt_required()
     def delete(self, id: int):
         return delete_table(id)
 
+    @jwt_required()
     def patch(self, id: int):
         return update_table(id)
 
 
 class TableLoginResource(Resource):
     def post(self):
-        return login_table()
+        try:
+            return login_table()
+        except IntegrityError:
+            return {"error": "Wrong lenght of parameters"}, HTTPStatus.BAD_REQUEST
