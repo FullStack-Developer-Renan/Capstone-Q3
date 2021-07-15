fa9e18e2a4d66d27b646f3b0a6fce1e54496c404
Luciano Feder <lucianoo.feder@gmail.com>
2021-07-15 12:31:40 +0000

    feat(app/models/orders_model && restaurant_table_model): create...

--- app/services/tables_services.py
+++ app/services/tables_services.py
@@ -1,9 +1,12 @@
 from flask import request, jsonify
+from werkzeug.exceptions import NotFound
 from .helpers import add_commit, delete_commit
 from flask_restful import reqparse
 from app.models.restaurant_table_model import RestaurantTableModel
 from http import HTTPStatus
 from flask_jwt_extended import create_access_token
+from app.exc import DuplicatedKeys
+
 
 def get_all() -> list:
     table = RestaurantTableModel.query.all()
@@ -11,16 +14,10 @@ def get_all() -> list:
     response = []
 
     for value in table:
-        response.append(
-            {
-                "id": value.id,
-                "seats": value.seats,
-                "number": value.number,
-                "total": value.total,
-                "empty": value.empty,
-            }
-        )
-    
+        value_serialize = value.serialize()
+        value_serialize["orders_list"] = f"/api/orders?table_id={value.id}"
+        response.append(value_serialize)
+
     return response, HTTPStatus.OK
 
 def get_all():
@@ -28,15 +25,28 @@ def get_all():
     return jsonify(table_list), HTTPStatus.OK
 
 
+def get_table_by_login(data) -> RestaurantTableModel:
+    table: RestaurantTableModel = RestaurantTableModel.query.filter_by(
+        login=data["login"]
+    ).first()
+    return table
+
+
 # endpoint(CREATE_TABLE) = '/api/tables/' -> POST
 def create_table() -> RestaurantTableModel:
+
     parser = reqparse.RequestParser()
 
     parser.add_argument("number", type=int, required=True)
     parser.add_argument("login", type=str, required=True)
     parser.add_argument("password", type=str, required=True)
 
-    new_table: RestaurantTableModel = RestaurantTableModel(**parser.parse_args())
+    data = parser.parse_args(strict=True)
+
+    if get_table_by_login(data):
+        raise DuplicatedKeys
+
+    new_table: RestaurantTableModel = RestaurantTableModel(**data)
 
     add_commit(new_table)
 
@@ -47,7 +57,6 @@ def create_table() -> RestaurantTableModel:
     }
 
 
-# endpoint(LOGIN_TABLE) = '/api/tables/login' -> POST
 def login_table():
 
     parser = reqparse.RequestParser()
@@ -55,41 +64,35 @@ def login_table():
     parser.add_argument("login", type=str, required=True)
     parser.add_argument("password", type=str, required=True)
 
-    data = parser.parse_args()
+    data = parser.parse_args(strict=True)
 
     user: RestaurantTableModel = RestaurantTableModel.query.filter_by(
         login=data["login"]
     ).first()
 
     if not user:
-        return {
-            "message": "User not Found"
-        }, HTTPStatus.NOT_FOUND
+        raise NotFound
 
-    if user.check_password(data['password']):
+    if user.check_password(data["password"]):
         token = create_access_token(identity=user)
-        return {
-            "token": token
-        }, HTTPStatus.OK
+        return {"token": token}, HTTPStatus.OK
     else:
         return {
             "message": "Invalid password or login information"
         }, HTTPStatus.UNAUTHORIZED
 
 
-# endpoint(DELETE_TABLE) = '/api/tables/<table_id: int>/' -> DELETE
 def delete_table(table_id) -> str:
     found_table = RestaurantTableModel.query.get(table_id)
 
     if not found_table:
-        return {"status": "table not found"}, HTTPStatus.NOT_FOUND
+        raise NotFound
 
     delete_commit(found_table)
 
     return "", HTTPStatus.NO_CONTENT
 
 
-# endpoint(UPDATE_TABLE) = '/api/tables/<table_id: int>/' -> PATCH
 def update_table(table_id: int) -> RestaurantTableModel:
     parser = reqparse.RequestParser()
 
@@ -105,7 +108,7 @@ def update_table(table_id: int) -> RestaurantTableModel:
     table = RestaurantTableModel.query.get(table_id)
 
     if not table:
-        return {"status": "table not found"}, HTTPStatus.NOT_FOUND
+        raise ("Error")
 
     for key, value in data.items():
         if value != None:
@@ -113,33 +116,18 @@ def update_table(table_id: int) -> RestaurantTableModel:
 
     add_commit(table)
 
-    return {
-        "id": table.id,
-        "seats": table.seats,
-        "number": table.number,
-        "total": table.total,
-        "empty": table.empty,
-        "login": table.login,
-    }, HTTPStatus.OK
+    return table.serialize(), HTTPStatus.OK
 
 
-# endpoint(GET_TABLE) = '/api/tables/<table_id: int>/' -> GET
 def get_by_id(table_id) -> RestaurantTableModel:
     table = RestaurantTableModel.query.get(table_id)
 
     if table:
-        return {
-            "id": table.id,
-            "seats": table.seats,
-            "number": table.number,
-            "total": table.total,
-            "empty": table.empty,
-        }, HTTPStatus.OK
+        return table.serialize(), HTTPStatus.OK
 
     return {"status": "table not found!"}, HTTPStatus.NOT_FOUND
 
 
-# endpoint(GET_TABLES) = '/api/tables?empty=<empty: bool>/' -> GET
 def get_tables() -> dict:
     args = request.args
     response = []
@@ -151,15 +139,7 @@ def get_tables() -> dict:
 
         list = []
         for table in response:
-            list.append(
-                {
-                    "id": table.id,
-                    "seats": table.seats,
-                    "number": table.number,
-                    "total": table.total,
-                    "empty": table.empty,
-                }
-            )
+            list.append(table.serialize())
 
         return list
 
--- app/views/orders_views.py
+++ app/views/orders_views.py
@@ -7,8 +7,10 @@ from sqlalchemy.sql.expression import table
 from flask import request
 from sqlalchemy.exc import DataError, IntegrityError
 from app.services.orders_services import create_order,get_current_orders, get_orders, get_order, remove_order,update_order
+from flask_jwt_extended import jwt_required
 
 class OrdersResource(Resource):
+    @jwt_required()
     def post(self):
         try:
             return create_order(),HTTPStatus.CREATED
@@ -16,7 +18,7 @@ class OrdersResource(Resource):
             return {"Message": "Table_Id doesn't exist"}, HTTPStatus.UNPROCESSABLE_ENTITY
         except DataError as _:
             return {"Message": "Invalid parameter value!"}, HTTPStatus.UNPROCESSABLE_ENTITY
-    
+    @jwt_required
     def get(self):
         
         try:
@@ -27,9 +29,11 @@ class OrdersResource(Resource):
 
 
 class OrderIDResource(Resource):
+    @jwt_required
     def get(self, order_id: int):
         return get_order(order_id)
         
+    @jwt_required
     def delete(self, order_id: int):
         try:
             return remove_order(order_id), HTTPStatus.NO_CONTENT
@@ -40,6 +44,7 @@ class OrderIDResource(Resource):
         except AttributeError as _:
             return {"error":"Order doesn't exists"}, HTTPStatus.UNPROCESSABLE_ENTITY
     
+    @jwt_required
     def patch(self, order_id: int):
         try:
             return update_order(order_id), HTTPStatus.CREATED
